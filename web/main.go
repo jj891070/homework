f492ccb0ef89ef010cdd01d0d92d8bc2dec80c9b
jay_liao <chungyojay0206@gmail.com>
2018-08-09 11:31:57 +0800

    for test features part2

---
+++ web/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+)
+
+type User struct {
+	Username string `form:"username" json:"username" binding:"required"`
+	Passwd   string `form:"passwd" json:"passwd" bdinding:"required"`
+	Age      int    `form:"age" json:"age"`
+}
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/login", func(c *gin.Context) {
+		var user User
+		var err error
+		contentType := c.Request.Header.Get("Content-Type")
+
+		switch contentType {
+		case "application/json":
+			err = c.BindJSON(&user)
+		case "application/x-www-form-urlencoded":
+			err = c.BindWith(&user, binding.Form)
+		}
+
+		if err != nil {
+			fmt.Println(err)
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"user":   user.Username,
+			"passwd": user.Passwd,
+			"age":    user.Age,
+		})
+
+	})
+
+	router.Run(":8087")
+
+}
+
+func routeJson(c *gin.Context) {
+	err := c.BindJSON(&user)
+	if err != nil {
+		return
+	}
+}
